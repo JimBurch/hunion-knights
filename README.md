b1fcdd17233f8b0f5350ba2aaf776209c36e1aca
Jim Burch <jrburch1@gmail.com>
2021-07-30 21:03:36 -0700

    discord.js guide updates

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hunion-knights
+
 A support Discord bot for the Hunion Knights company
+
+### Documentation
+
+https://www.notion.so/Hunion-Knights-Discord-Bot-6b6c99729a2f45529ab2cff8bcc081a4
--- index.js
+++ index.js
@@ -5,21 +5,59 @@ const client = new Discord.Client({
 	partials: ['MESSAGE'],
 });
 
-const { BOT_TOKEN } = process.env;
+const { PREFIX, BOT_TOKEN } = process.env;
 
 client.on('ready', () => {
-	console.log('Ready for battle!');
+	console.log('Hunion Bot, ready for battle!');
 });
 
-client.on('message', msg => {
-	if (msg.content === 'are we ready for battle?') {
-		msg.reply('hell yeah');
-	} else if (msg.content === 'u good bro') {
-		msg.channel.send('nah');
-	} else if (msg.content === 'down with syndicate') {
-		msg.react('❤️');
-	} else if (msg.content === 'gimme mod') {
-		msg.member.roles.add('870852668862636082');
+client.on('message', message => {
+	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'args-info') {
+		if (!args.length) {
+			return message.channel.send(
+				`You didn't provide any arguments, ${message.author}!`
+			);
+		} else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+		message.channel.send(`First argument: ${args[0]}`);
+	} else if (command === 'kick') {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
+		}
+
+		// Grab the "first" mentioned user from the message
+		// This will return a `User` object, just like `message.author`
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	} else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(
+				`Your avatar: <${message.author.displayAvatarURL({
+					format: 'png',
+					dynamic: true,
+				})}>`
+			);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL({
+				format: 'png',
+				dynamic: true,
+			})}>`;
+		});
+
+		// Send the entire array of strings as a message
+		// By default, discord.js will `.join()` the array with `\n`
+		message.channel.send(avatarList);
 	}
 });
 
